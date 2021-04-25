e61a1942d9b28c9a1f6096575528e4c9ae5916a5
lminhtam <lminhtam99@gmail.com>
2021-04-25 21:47:42 +0700

    init commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import Home from 'pages/Home/home.page'
+import React from 'react'
+import { HashRouter as Router, Route } from 'react-router-dom'
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <Route exact path="/" component={Home} />
+    </Router>
+  )
 }
 
-export default App;
+export default App
